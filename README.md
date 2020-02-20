6322842da02d77999755ba941ffc0ac778fdbffd
Lucas Matheus <lucasmatheus2021@gmail.com>
2020-02-20 20:13:12 -0300

    v1.0
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

---
+++ README.md
@@ -0,0 +1,23 @@
+# CalculatorX
+
+> react-native project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# to start server at localhost:19002 default
+npm start or expo start (if you are using expo framework)
+
+# to start a server with android expo sdk
+expo start --android
+# to start a server with ios expo sdk
+expo start --ios
+# to start a web expo server
+expo start --web
+# to run eslint
+eslint [FILE_NAME].js
+
+```
--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -6,87 +6,115 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import normalize from 'react-native-normalize';
+
 export default class Main extends Component {
   constructor (props) {
     super(props);
     this.state = {
       isLoading: false,
-      panelValue: 0,
+      numberKey: [],
+      defaultPanelValue: 0,
     }
   }
 
-  getKeyValue = () => {
-    //bla bla bla
+  getKeyValue = (value) => {
+    if( this.state.numberKey.length < 7 )
+      this.setState({ numberKey: this.state.numberKey + value });
+    this.onPressNumber(value)
   };
+
+  onPressNumber = (value) => {
+    if (value == 'AC')
+      this.setState({ numberKey: '', defaultPanelValue: 0, });
+    else if (this.state.numberKey.includes(' ')) {
+      let result = eval(this.state.numberKey)
+      this.setState({ numberKey: '', defaultPanelValue: result, });
+    }
+  }
+
+  // componentDidUpdate = () => {
+  //     this.onPressNumber();
+  // };
 
   render () {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <View style={{ alignSelf:'center', marginVertical:75, }}>
+          <View style={{ alignSelf:'center', marginVertical: normalize(75, 'height'), }}>
             <Text style={{ color:'#fff', fontSize:28, fontWeight:'bold', letterSpacing:2, }}> CALCULATORX </Text>
           </View>
         </View>
         <View style={styles.display}>
+          {
+          this.state.numberKey!=''?
+          <View>
+            <TouchableOpacity onPress={ () => this.getKeyValue('AC') } style={{ position:'absolute', top:20, left:-10, }}>
+              <Text style={{ fontSize:19, color:'#000', }}>clear</Text>
+            </TouchableOpacity>
+          </View>
+          :
+          null
+          }
           <View style={{ alignSelf:'flex-end', }}>
-            <Text style={{ color:'#000', fontSize:52, }}>{this.state.panelValue}</Text>
+            <Text style={{ color:'#000', fontSize:52, }}>{this.state.numberKey!=''?this.state.numberKey:this.state.defaultPanelValue}</Text>
           </View>
         </View>
         <View style={styles.keysPart}>
           <View style={{ ...styles.keysOrientation, flexDirection:'row', alignSelf:'center', }}>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue('+') }>
               <Text style={{ ...styles.keys }}>+</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue('-') }>
               <Text style={{ ...styles.keys }}>-</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue('*') }>
               <Text style={{ ...styles.keys }}>x</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue('/') }>
               <Text style={{ ...styles.keys }}>÷</Text>
             </TouchableOpacity>
           </View>
           <View style={{ ...styles.keysOrientation, flexDirection:'row', alignSelf:'center', }}>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(7) }>
               <Text style={{ ...styles.keys }}>7</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(8) }>
               <Text style={{ ...styles.keys }}>8</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(9) }>
               <Text style={{ ...styles.keys }}>9</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(0) }>
               <Text style={{ ...styles.keys }}>0</Text>
             </TouchableOpacity>
           </View>
           <View style={{ ...styles.keysOrientation, flexDirection:'row', alignSelf:'center', }}>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(4) }>
               <Text style={{ ...styles.keys }}>4</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(5) }>
               <Text style={{ ...styles.keys }}>5</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(6) }>
               <Text style={{ ...styles.keys }}>6</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue('.') }>
               <Text style={{ ...styles.keys }}>.</Text>
             </TouchableOpacity>
           </View>
           <View style={{ ...styles.keysOrientation, flexDirection:'row', alignSelf:'center', }}>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(1) }>
               <Text style={{ ...styles.keys }}>1</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(2) }>
               <Text style={{ ...styles.keys }}>2</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
+            <TouchableOpacity onPress={ () => this.getKeyValue(3) }>
               <Text style={{ ...styles.keys }}>3</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={(value) => { this.getKeyValue(value) }}>
-              <Text style={{ ...styles.keys, backgroundColor:'#fd48a9', }}>=</Text>
+            <TouchableOpacity onPress={ () => this.getKeyValue(' ') } disabled={this.state.numberKey!=''?false:true} >
+              <Text style={{ ...styles.keys, backgroundColor:'#fd48a9', borderRadius:150, }}>=</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -104,13 +132,13 @@ const styles = StyleSheet.create({
   header: {
     backgroundColor:'#e4273d',
     width:'100%',
-    height:150,
+    height: normalize(150, 'height'),
   },
   display: {
     backgroundColor:'#fff',
     paddingHorizontal:30,
     width:'100%',
-    padding:60,
+    padding: normalize(60, 'height'),
   },
   keysPart: {
     backgroundColor:'#272cd0',
@@ -122,11 +150,11 @@ const styles = StyleSheet.create({
     color:'#fff',
     textAlign:'center',
     width:80,
-    marginHorizontal:10,
+    marginHorizontal: normalize(10, 'width'),
     marginTop:40,
     padding:10,
   },
   keysOrientation: {
-    top:10,
+    top: normalize(10, 'height'),
   },
 });
